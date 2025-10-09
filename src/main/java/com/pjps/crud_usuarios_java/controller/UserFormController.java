2feb4eba985032a7b09aa2954c1451c8989436d5
Pablo Júnio Penha Santos <lpablosojunio@gmail.com>
2025-10-09 20:54:56 -0300

    adicionado .env

--- src/main/java/com/pjps/crud_usuarios_java/controller/UserFormController.java
+++ src/main/java/com/pjps/crud_usuarios_java/controller/UserFormController.java
@@ -1,4 +1,53 @@
 package com.pjps.crud_usuarios_java.controller;
 
+
+import com.pjps.crud_usuarios_java.model.Usuario;
+import com.pjps.crud_usuarios_java.service.UsuarioService;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class UserFormController {
+    @FXML
+    private Label titleLable;
+    @FXML
+    private TextField nomeField;
+    @FXML
+    private TextField sobrenome;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private TextField telefoneField;
+    @FXML
+    private DatePicker dataNascimentoPicker;
+    @FXML
+    private ChoiceBox<String> sexoChoiceBox;
+    @FXML
+    private TextField enderecoField;
+
+    private Stage stage;
+    private Usuario usuario;
+    private UsuarioService usuarioService;
+
+    public void initialize() {
+
+    }
+
+    public void setStage() {
+
+    }
+
+    public void setUsuario() {
+
+    }
+
+    @FXML
+    public void handleSalvar() {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,9 +8,10 @@ module com.pjps.crud_usuarios_java {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.pjps.crud_usuarios_java to javafx.fxml;
-    opens com.pjsp.crud_usuarios_java.controller to javafx.fxml;
+    opens com.pjps.crud_usuarios_java.controller to javafx.fxml;
     opens com.pjps.crud_usuarios_java.model to javafx.base;
 
     exports com.pjps.crud_usuarios_java;
